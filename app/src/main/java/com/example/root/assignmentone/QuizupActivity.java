75561106e193afb224e2c1c0d47ac3f60b20787b
PrinceP <prince15046@iiitd.ac.in>
2016-08-14 23:21:07 +0530

    Updated the Layout.

--- app/src/main/java/com/example/root/assignmentone/QuizupActivity.java
+++ app/src/main/java/com/example/root/assignmentone/QuizupActivity.java
@@ -32,7 +32,7 @@ public class QuizupActivity extends AppCompatActivity {
 
     public boolean isPrime(){
 
-        for(int i = 2 ; i < randomNumber; i++){
+        for(int i = 2 ; i <= Math.sqrt(randomNumber); i++){
             if(randomNumber % i == 0)
                 return false;
         }
@@ -81,6 +81,31 @@ public class QuizupActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
 
